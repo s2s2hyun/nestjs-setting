d588618fb8d721dac609add6a6d779b20703177b
s2s2hyun <s2s2hyun0601@gmail.com>
2024-10-24 02:01:59 +0900

    크론사용방법 확인

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { MemberModule } from '@/modules/member/member.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot()], // 스케줄러 모듈 추가
+  imports: [ScheduleModule.forRoot(), MemberModule], // 스케줄러 모듈 추가
   providers: [TasksService], // 스케줄러 서비스 등록
 })
 export class TasksModule {}
