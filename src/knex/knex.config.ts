05db26243435430f641332575a284c3ed66b9d52
s2s2hyun <s2s2hyun0601@gmail.com>
2024-10-25 00:43:31 +0900

    env jwt, db

--- src/knex/knex.config.ts
+++ src/knex/knex.config.ts
@@ -1,14 +1,18 @@
 import { Knex } from 'knex';
+import * as dotenv from 'dotenv';
+
+// .env 파일에서 환경 변수 불러오기
+dotenv.config();
 
 export const knexConfig: Knex.Config = {
   client: 'mysql2',
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'yourpassword',
-    database: 'yourdatabase',
-    timezone: '+09:00',
-    connectTimeout: 30000,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    timezone: process.env.DB_TIMEZONE,
+    connectTimeout: Number(process.env.DB_CONNECT_TIMEOUT), // 숫자로 변환
   },
   debug: false,
 };
